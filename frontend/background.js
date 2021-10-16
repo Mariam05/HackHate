db4c002e8d73ac070ab8ecde4e433b1352dbcf70
Mariam05 <tomariam@gmail.com>
2021-10-16 16:02:56 -0400

    Added notification and db for domain count

--- frontend/background.js
+++ frontend/background.js
@@ -13,3 +13,27 @@ chrome.action.onClicked.addListener(function(tab) {
     setIcon(toggled);
   })
 });
+
+
+// to communicate with content script
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting === "hello" && !chrome.storage.local.disabled){
+      sendResponse({farewell: "goodbye"});
+      // chrome.action.setBadgeText({text: "yes"});
+      var notifOptions = {
+        type: 'basic',
+        iconUrl: 'images/sadheart.png',
+        title: 'we detected some hate speech on this page',
+        message: "sample messsage"
+      };
+      chrome.notifications.create('HateDetectedNotif', notifOptions);
+    }
+     
+  }
+);
+
+
